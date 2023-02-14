f8a74044289e55cabd52d7cf153af4be64ac450b
Cristian Ribeiro <fhr.cristian@gmail.com>
2023-02-14 16:41:36 -0300

    Add built site

---
+++ build/precache-manifest.7c5b76db24f2ed4655d7d60dbbf228c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b074b924ad8bc8c35922116716d81d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c5bedde7b88770cc2f",
+    "url": "/static/css/2.c1472be1.chunk.css"
+  },
+  {
+    "revision": "d78525192154e4a6b820",
+    "url": "/static/css/4.27a412b4.chunk.css"
+  },
+  {
+    "revision": "d0eb4be0fc4f99ddef9c",
+    "url": "/static/css/main.6f29b9aa.chunk.css"
+  },
+  {
+    "revision": "e1c5bedde7b88770cc2f",
+    "url": "/static/js/2.d175df45.chunk.js"
+  },
+  {
+    "revision": "30887c1754c1fe9ca9e1",
+    "url": "/static/js/3.21e716c2.chunk.js"
+  },
+  {
+    "revision": "bfc9d84b53089fbab6811529b6f9d662",
+    "url": "/static/js/3.21e716c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78525192154e4a6b820",
+    "url": "/static/js/4.1ded7cce.chunk.js"
+  },
+  {
+    "revision": "d0eb4be0fc4f99ddef9c",
+    "url": "/static/js/main.9aac230c.chunk.js"
+  },
+  {
+    "revision": "f04ae2d47c5bd8a98aa4",
+    "url": "/static/js/runtime-main.980e2a26.js"
+  }
+]);
